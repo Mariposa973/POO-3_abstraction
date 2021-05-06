efdd6fd41838f429a9a5fbf4371a49c84efed101
Sara P <sarapralier@gmail.com>
2021-05-06 13:54:00 +0200

    update

--- Speedometer.php
+++ Speedometer.php
@@ -4,12 +4,18 @@ namespace App;
 Class Speedometer
 {
     public const KM_TO_MILES_CONVERSION = 0.621371;
+    public const MILES_TO_KM_CONVERSION = 1.60934;
 
     public static function convertKmToMiles(int $kilometers): float
     {
         return $kilometers * self::KM_TO_MILES_CONVERSION;
     }
-
+    public static function convertMilesToKm(int $miles): float
+    {
+        return $miles * self::MILES_TO_KM_CONVERSION;
+    }
 }
+
+
 
 echo Speedometer::convertKmToMiles(10);
