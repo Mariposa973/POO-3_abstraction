9302e1d008e79f326ff992b931686034887d52e7
Sara P <sarapralier@gmail.com>
2021-05-06 13:50:08 +0200

    classe Speedometer avec méthode static

---
+++ Speedometer.php
@@ -0,0 +1,15 @@
+<?php 
+namespace App;
+
+Class Speedometer
+{
+    public const KM_TO_MILES_CONVERSION = 0.621371;
+
+    public static function convertKmToMiles(int $kilometers): float
+    {
+        return $kilometers * self::KM_TO_MILES_CONVERSION;
+    }
+
+}
+
+echo Speedometer::convertKmToMiles(10);
